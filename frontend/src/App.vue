2898dc6bd1514436331db169019d40b52b410ac0
markus <datamastery87@gmail.com>
2020-08-15 19:19:04 +0200

    basic frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <navbar />
+
+    <v-main></v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+
+  components: {
+    navbar: Navbar
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card color="grey lighten-4" flat tile>
+    <v-toolbar color="#229495" dark>
+      <v-toolbar-title>CompuNerds</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-for="item in left_nav" :key="item.icon" :title="item.title" text>{{ item.text }}</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        Login
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn color="primary" dark>
+        Cart
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      left_nav: [
+        { text: "All Products" },
+        { text: "Smartphones" },
+        { text: "Notebooks" }
+      ]
+    };
+  }
+};
+</script>
+<style>
+</style>
