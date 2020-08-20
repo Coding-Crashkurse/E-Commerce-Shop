8899b92780210cfe79e49bdc6f68b863f88a6720
markus <datamastery87@gmail.com>
2020-08-20 20:57:02 +0200

    added dynamic table

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,42 +1,27 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="6">
-        <template>
-          <v-card class="mx-auto" max-width="420" tile>
-            <v-img
-              height="100%"
-              src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-            >
-              <v-row align="end" class="fill-height">
-                <v-col align-self="start" class="pa-0" cols="12">
-                  <v-avatar class="profile" color="grey" size="164" tile>
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
-                    ></v-img>
-                  </v-avatar>
-                </v-col>
-                <v-col class="py-0">
-                  <v-list-item color="rgba(0, 0, 0, .4)" dark>
-                    <v-list-item-content>
-                      <v-list-item-title class="title">{{
-                        username
-                      }}</v-list-item-title>
-                      <v-list-item-subtitle
-                        >Network Engineer</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-col>
-              </v-row>
-            </v-img>
-          </v-card>
-        </template>
-      </v-col>
-    </v-row>
-    <v-row>
       <v-col cols="12" id="history">
-        <h3></h3>
+        <h3>
+          Hello
+          <strong>{{this.username}}</strong>
+          , you made {{this.history.length}} purchases in the past:
+        </h3>
+        <v-spacer></v-spacer>
+        <v-btn>Log out</v-btn>
+        <table id="customers">
+          <tr>
+            <th>Item</th>
+            <th>Price</th>
+            <th>Purchase Date</th>
+          </tr>
+          <tr v-for="(item, index) in history" :key="index">
+            <td>{{item.item}}</td>
+            <td>{{item.price}}</td>
+            <td>{{item.date}}</td>
+          </tr>
+        </table>
+        <h3>{{totalPrice}}</h3>
       </v-col>
     </v-row>
   </v-container>
@@ -58,15 +43,49 @@ export default {
       { item: "Laptop xyz", price: 129.99, date: new Date() },
       { item: "Samsung Galaxy S8", price: 129.99, date: new Date() },
       { item: "IPad Pro", price: 129.99, date: new Date() },
-      { item: "PC", price: 89.99, date: new Date() }
+      { item: "PC", price: 89.99, date: new Date() },
+      { item: "MacBook Mini", price: 599.99, date: new Date() }
     ]
-  })
+  }),
+  computed: {
+    totalPrice() {
+      const total = this.history.reduce(function(acc, obj) {
+        return acc + obj.price;
+      }, 0);
+      return `Total spendings ${total} $`;
+    }
+  }
 };
 </script>
 
 <style scoped>
-#history {
-  margin-top: 50px;
-  text-align: center;
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  margin: 15px 0px;
+}
+
+#customers td,
+#customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+  width: 33.33%;
+}
+
+#customers tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#customers tr:hover {
+  background-color: #ddd;
+}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #229495;
+  color: white;
 }
 </style>
