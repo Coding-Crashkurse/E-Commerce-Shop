820a496017d714f4e334b0d9560821845d65baec
markus <datamastery87@gmail.com>
2020-08-21 21:21:37 +0200

    add vuex login

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VueJwtDecode from "vue-jwt-decode";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    loggedIn: false
+    loggedIn: false,
+    username: ""
   },
   mutations: {
     logIn(state, data) {
       state.loggedIn = data;
+      const token = window.localStorage.getItem("token");
+      const result = VueJwtDecode.decode(token);
+      state.username = result;
     }
   }
 });
